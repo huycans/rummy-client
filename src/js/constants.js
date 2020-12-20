cb3d28b6f003c954e15efc60a12514de00574ec8
huycans <huycan_lk@yahoo.com.vn>
2020-12-20 10:42:34 -0500

    Disable https URL

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,4 +1,4 @@
-const SERVER_URL = process.env.SERVER_URL || "https://localhost:3000";
+const SERVER_URL = process.env.SERVER_URL || "http://localhost:3000";
 
 const SERVER_API = {
   SIGNIN: "/user/signin",
